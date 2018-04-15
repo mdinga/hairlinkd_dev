45c0fdc93c2444dcd1ecad7e6038eca9d8d4222f
Mbasa <mbasa.dinga@gmail.com>
2018-04-15 21:24:35 +0200

    Contact Deleted

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,15 +1,3 @@
 class Contact < Mailform::Base
-  attribute :name,      :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :file,      :attachment => true
-  attribute :message    :validate => true
-  attribute :nickname,  :captcha  => true
 
-  def headers
-  {
-    :subject => "My Contact Form",
-    :to => "mbasa.dinga@gmail.com",
-    :from => %("#{name}" <#{email}>)
-  }
-end
 end
