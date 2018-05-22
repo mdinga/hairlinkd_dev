b15419ee62084fa2da74f78e3cc0b861c631310f
Mbasa <mbasa.dinga@gmail.com>
2018-05-22 21:44:41 +0200

    edit deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:mdinga/hairlinkd_dev.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
- set :deploy_to, "home/deployer/hairlinkd"
+ set :deploy_to, "/home/deployer/hairlinkd"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
