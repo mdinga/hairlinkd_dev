82216813ec5d5d962ca9a06ab01fe79f8a41c8b9
Mbasa <mbasa.dinga@gmail.com>
2018-07-01 21:39:48 +0200

    Testing Mailer

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -23,7 +23,7 @@ class ClientsController < ApplicationController
     @client.add_role :def_client
     if @client.save
       flash[:notice] = "Profile Created Successfully, Please Log In"
-      #RegisterMailer.new_client.deliver_now
+      RegisterMailer.new_client.deliver_now
       redirect_to (client_access_login_path)
     else
       render ('new')
