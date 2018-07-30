6e167156f903cc3f2de6774a24ea614898432ff8
Mbasa <mbasa.dinga@gmail.com>
2018-07-30 13:40:09 +0200

    reverting changes

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,14 +1,3 @@
 class Conversation < ApplicationRecord
-  belongs_to :author, class_name: "User"
-  belongs_to :receiver class_name: "User"
-  has_many :personal_messages, -> {order(created_at: :asc)}, dependent: :destroy
-
-  scope :participating, lambda{|user|, where("(conversation.author.id = ? OR conversation.receiver.id = ?)", user.id, user.id)}
-
-  validates :author, uniqueness: {scope: :reciever}
-
-  def with(current_user)
-    author == current_user.user ? reciever : author
-  end
-
+ 
 end
--- app/models/personal_message.rb
+++ app/models/personal_message.rb
@@ -1,6 +1,3 @@
 class PersonalMessage < ApplicationRecord
-  belongs_to :conversation
-  belongs_to :user
 
-  validates :body, presence: true
 end
